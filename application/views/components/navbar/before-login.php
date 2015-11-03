97716b56d46f8c2e9eafc69d7861b8238065f43c
Robert Limanto <r_limanto@yahoo.com>
2015-11-03 16:11:57 +0800

    login from navbar

--- application/views/components/navbar/before-login.php
+++ application/views/components/navbar/before-login.php
@@ -51,15 +51,15 @@
  							<a class="dropdown-toggle" href="#" data-toggle="dropdown">Masuk <strong class="caret"></strong></a>
  							<div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
  								<!-- Login form here -->
- 								<form action="/startUp/login.php" method="post" accept-charset="UTF-8">
+ 								<form action="navbarcontroller.php" method="post" accept-charset="UTF-8">
  									<input id="user_username" style="margin-bottom: 15px;" type="text" name="email" size="30" placeholder="Email"/>
  									<input id="user_password" style="margin-bottom: 15px;" type="password" name="password" size="30" placeholder="Password"/>
                                   <div class="form-group">
  									<input id="user_remember_me" style="float: left; margin-right: 10px;" type="checkbox" name="user[remember_me]" value="1" />
  									<label class="string optional" for="user_remember_me"> Remember me</label>
-                                     <a style="float:right" href="sign_up.php">Daftar</a>
+                                     <a style="float:right" href="register.php">Daftar</a>
                                   </div>
- 									<input class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px;" type="submit" name="commit" value="Sign In" />
+ 									<input class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px;" type="submit" name="login" value="Sign In" />
  								</form>
  							</div>
  						</li>
