16838fcaec4358f1cfbd7b473fb034fff1e02389
gchristi001 <gracechristina05@gmail.com>
2015-12-28 00:26:22 +0800

    Profile Doneee :)

--- frontend/controllers/ProfileController.php
+++ frontend/controllers/ProfileController.php
@@ -27,32 +27,14 @@ class ProfileController extends Controller
         if ($model->load(Yii::$app->request->post())) {
             if ($user = $model->edit()) {
                 if (Yii::$app->getUser()->login($user)) {
-                    return $this->render('edit-confirm', [
-            				'model' => $model,
-        ]);
+                    return $this->render('edit-confirm');
                 }
             }
         }
-     }
- }
-/*
-<<<<<<< HEAD
+
         return $this->render('edit', [
             'model' => $model,
         ]);
-=======
-    	if($model ->load(Yii::$app->request->post()) && $model->validate()){
-    		if($user = $model->edit()){
-    			if (Yii::$app->getUser()->login($user)) {
-    		//add sth here
-    		return $this->render('edit-confirm',['model'=>$model]);
-
-    			}
-    		}
-    	} else{
-    		return $this->render('edit-profile',['model'=>$model]);
-    	}
->>>>>>> parent of 64af895... Edit Profile not complete
     }
-}
-*/
+ }
+    
