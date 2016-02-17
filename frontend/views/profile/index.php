7e8105c8533fb77bb436c1db942fae0bb83c2dec
MaX121 <admin@max-metal.us>
2016-02-18 00:33:28 +0700

    [UPDATE] profile page

--- frontend/views/profile/index.php
+++ frontend/views/profile/index.php
@@ -10,117 +10,71 @@ use yii\helpers\Url;
 use yii\bootstrap\Modal;
 use common\models\User;
 
-/*
-if($user['id'] == \Yii::$app->user->identity->getId() ){
-    $belongs = 1;
-}
-else{
-    $belongs = 0;
-}
-*/
 //Set title for the page
 $this->title = $user->first_name . ' ' . $user->last_name;
 ?>
+
 <!-- Edit Profile Modal-->
 <?php
-    Modal::begin([
-        'id' => 'editModal',
-        'size' => 'modal-lg'
-    ]);
+Modal::begin([
+    'id' => 'editModal',
+    'size' => 'modal-lg'
+]);
 
-    $editProfileModel = new \frontend\models\EditProfileForm();
-    $editUser = new User();
-  //  $editUser->id = \Yii::$app->user->identity->getId();
-    $user_data = $editUser->getUser();
+$editProfileModel = new \frontend\models\EditProfileForm();
+$editUser = new User();
+// $editUser->id = \Yii::$app->user->identity->getId();
+$user_data = $editUser->getUser();
 
-    //pass data
-    $editProfileModel->occupation = $user_data['occupation'];
-    $editProfileModel->birthday = $user_data['birthday'];
-    $editProfileModel->first_name = $user_data['first_name'];
-    $editProfileModel->last_name = $user_data['last_name'];
-    echo $this->render('edit', ['model' => $editProfileModel]);
-
-    Modal::end();
+// pass data
+$editProfileModel->occupation = $user_data['occupation'];
+$editProfileModel->birthday = $user_data['birthday'];
+$editProfileModel->first_name = $user_data['first_name'];
+$editProfileModel->last_name = $user_data['last_name'];
+echo $this->render('edit', ['model' => $editProfileModel]);
+Modal::end();
 ?>
-
 
 <!-- Upload photo modal -->
 <?php
-    Modal::begin([
-        'id' => 'uploadProfilePicModal',
-        'size' => 'modal-lg'
-    ]);
-
-    $uploadFormModel = new \frontend\models\UploadProfilePicForm();
-
-    echo $this->render('_upload_photo_modal', ['model' => $uploadFormModel]);
-
-
-    Modal::end();
-
-
+Modal::begin([
+    'id' => 'uploadProfilePicModal',
+    'size' => 'modal-lg'
+]);
+$uploadFormModel = new \frontend\models\UploadProfilePicForm();
+echo $this->render('_upload_photo_modal', ['model' => $uploadFormModel]);
+Modal::end();
 ?>
-
-
-     <div id="wrapper">
-
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#">
-                        Feeds
-                    </a>
-                </li>
-                <li> 
-                    <a href="#">All Activity</a>
-                </li>
-                <li>
-                    <a href="#">All tags</a>
-                </li>
-
-            </ul>
+<!-- Page Content -->
+<div class="container" style="margin-left: 0; margin-right: 0;">
+    <div class="row">
+        <div class="col-xs-6 col-md-3">
+            <nav class="navbar navbar-default sidebar" role="navigation">
+                <ul class="nav nav-pills nav-stacked">
+                    <li class="sidebar-brand"><a href="#">Feeds</a></li>
+                    <li><a href="#">All Activity</a></li>
+                    <li><a href="#">All tags</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col-xs-6 col-md-2">
+            <img class="img-rounded" src="<?= Yii::getAlias('@image_dir') . '/' . $user->photo_path ?>" alt="Profile Picture" style="width:148px;height:148px;" id="avatar">
+            <?= Html::button('Upload Photo', ['onclick' => 'beginProfilePicModal()', 'class' => 'btn btn-primary', 'id' => 'upload-image']) ?>
         </div>
 
+        <div class="col-xs-12 col-lg-4">
+            <div id="displayName"><?= $user->first_name ?> <?= $user->last_name ?></div><hr />
+            <table id="bio-table" class="table table-responsive" style="border: 0;">
+                <tr><td>Birthday</td><td><?= $user->birthday ?></td></tr>
+                <tr><td>Occupation</td><td><?= $user->occupation ?></td></tr>
+            </table>
+        </div>
 
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="col-lg-3">
-                            <img src="<?= Yii::getAlias('@image_dir') . '/' . $user->photo_path ?>" alt="Profile Picture" style="width:180px;height:180px;">
-                            <?= Html::button('Upload Photo', ['onclick' => 'beginProfilePicModal()', 'class' => 'btn btn-primary']) ?>
-                        </div>
-                        <div class="col-lg-6">
-                            <div id="displayName">
-                                <?= $user->first_name ?>
-                                <?= $user->last_name ?>
-                            </div>
-                            <div >
-                                Birthday: <?= $user->birthday ?>
+        <div class="col-lg-3">
+        </div>
+    </div>
+</div>
 
-                            </div>
-                            <div >
-                                Occupation: <?= $user->occupation ?>
-
-                            </div>
-                            <hr>
-
-                        </div>
-                        <div class="col-lg-3">
-
-
-                        </div>
-
-                    </div>
-                </div>
-<div class="col-md-12">
-
-
-<?php $this->registerJsFile(Yii::$app->request->baseUrl.'/js/profile-index.js'); ?>
-
-
-
-
-    
+<div class="col-xs-12">
+    <?php $this->registerJsFile(Yii::$app->request->baseUrl.'/js/profile-index.js'); ?>
+</div>
--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -1,27 +1,35 @@
 $(document).ready(function() {
     $("img#avatar").mouseenter(function() {
         $("img#avatar").css("opacity",".5");
-        $("div#upload-image").css("opacity","1");
+        $("button#upload-image").css("opacity","1");
     });
 
-    $("div#upload-image").mouseenter(function() {
+    $("button#upload-image").mouseenter(function() {
         $("img#avatar").css("opacity",".5");
-        $("div#upload-image").css("opacity","1");
+        $("button#upload-image").css("opacity","1");
     });
 
-    $("div#upload-image").mouseout(function() {
+    $("button#upload-image").mouseout(function() {
         $("img#avatar").css("opacity","1");
-        $("div#upload-image").css("opacity","0");
+        $("button#upload-image").css("opacity","0");
     });
 
     $("img#avatar").mouseout(function() {
         $("img#avatar").css("opacity","1");
-        $("div#upload-image").css("opacity","0");
+        $("button#upload-image").css("opacity","0");
     });
 
     $("#loginMenu").click(function(){
         $("#loginModal").modal("show")
             .find('#loginModal')
             .load($(this).attr("value"));
+    });
+
+    $("button#create-button").mouseenter(function(){
+        $('span#create-button-label').css("text-decoration","underline");
+    });
+
+    $("button#create-button").mouseleave(function(){
+        $('span#create-button-label').css("text-decoration","none");
     });
 })
