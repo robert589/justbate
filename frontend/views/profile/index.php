9138345c6cc3271ffbb50e79311eef1c4a90f0ae
MaX121 <admin@max-metal.us>
2016-02-24 15:14:42 +0700

    [UPDATE] Add tab for followers, following, and main activity

--- frontend/views/profile/index.php
+++ frontend/views/profile/index.php
@@ -5,6 +5,8 @@
 use yii\helpers\Html;
 use yii\widgets\ListView;
 use kartik\select2\Select2;
+use kartik\tabs\TabsX;
+use yii\data\ArrayDataProvider;
 use yii\widgets\ActiveForm;
 use yii\helpers\Url;
 use yii\bootstrap\Modal;
@@ -33,6 +35,67 @@ $editProfileModel->first_name = $user_data['first_name'];
 $editProfileModel->last_name = $user_data['last_name'];
 echo $this->render('edit', ['model' => $editProfileModel]);
 Modal::end();
+?>
+
+<!-- array definiton -->
+<?php
+$following_x = new \yii\data\ArrayDataProvider([
+	'allModels' => $followees,
+	'pagination' => [
+		'pageSize' => 10,
+	]
+]);
+
+$followers_x = new \yii\data\ArrayDataProvider([
+	'allModels' => $followers,
+	'pagination' => [
+		'pageSize' => 10,
+	]
+]);
+
+$item = [
+	[
+		'label' => 'Main Activity',
+		'content' => ListView::widget([
+			'dataProvider' => $followers_x,
+			'summary' => false,
+			'itemOptions' => ['class' => 'item'],
+			'layout' => "{summary}\n{items}\n{pager}",
+			'itemView' => function ($model, $key, $index, $widget) {
+				return $this->render('_list_follower', ['model' => $model]);
+			}
+		]),
+		'active' => false,
+	],
+
+	[
+		'label' => 'Following',
+		'content' => ListView::widget([
+			'dataProvider' => $following_x,
+			'summary' => false,
+			'itemOptions' => ['class' => 'item'],
+			'layout' => "{summary}\n{items}\n{pager}",
+			'itemView' => function ($model, $key, $index, $widget) {
+				return $this->render('_list_follower', ['model' => $model]);
+			}
+		]),
+		'active' => true,
+	],
+
+	[
+		'label' => 'Follower',
+		'content' => ListView::widget([
+			'dataProvider' => $follower_x,
+			'summary' => false,
+			'itemOptions' => ['class' => 'item'],
+			'layout' => "{summary}\n{items}\n{pager}",
+			'itemView' => function ($model, $key, $index, $widget) {
+				return $this->render('_list_follower', ['model' => $model]);
+			}
+		]),
+		'active' => false,
+	]
+];
 ?>
 
 <!-- Upload photo modal -->
@@ -64,51 +127,26 @@ Modal::end();
 			<?= Html::button('Upload Photo', ['onclick' => 'beginProfilePicModal()', 'class' => 'btn btn-primary', 'id' => 'upload-image']) ?>
 		</div>
 
-		<div class="col-xs-12 col-md-4">
-			<div id="displayName"><?= $user->first_name ?> <?= $user->last_name ?></div><hr />
+		<div class="col-xs-12 col-md-3">
+			<div id="displayName"><?= $user->first_name ?> <?= $user->last_name ?><button style="float: right;" class="btn btn-primary">Follow</button></div><hr />
 			<table id="bio-table" class="table table-responsive">
 				<tr><td>Birthday</td><td><?= $user->birthday ?></td></tr>
 				<tr><td>Occupation</td><td><?= $user->occupation ?></td></tr>
 			</table>
-			</div>
 		</div>
 	</div>
-
 	<div class="container">
-		<div class="row">
-			<div class="col-xs-12" style="float: left;">
-				<table class="table table-responsive" style="text-align: center;" id="profile-details">
-					<tr><td><?= $num_following ?></td><td><?= $num_followers ?></td><td rowspan="2" style="vertical-align: middle; background: rgb(50,205,50); color: white; font-weight: bold;">FOLLOW <?= $user->first_name ?> <?= $user->last_name ?></td></tr>
-					<tr><td>Following</td><td>Follower</td></tr>
-				</table>
-			</div>
-			<div class="col-xs-12 col-md-6">
-				<table class="table table-responsive table-bordered" id="follower-table" style="text-align: center;">
-					<tr style="background: rgb(55,200,55);"><td>List of Follower</td></tr>
-					<?php
-					foreach($followees as $followee) {
-echo <<<HTML
-				<tr><td>$followee[first_name] $followee[last_name]</td></tr>
-HTML;
-}
-					?>
-				</table>
-			</div>
-			<div class="col-xs-12 col-md-6">
-				<table class="table table-responsive table-bordered" id="following-table" style="text-align: center;">
-					<tr style="background: rgb(55,55,200);"><td>List of Following</td></tr>
-					<?php
-					foreach($followers as $follower) {
-						echo <<<HTML
-				<tr><td>$follower[first_name] $follower[last_name]</td></tr>
-HTML;
-}
-					?>
-				</table>
+		<div class="col-md-offset-3 col-xs-12 col-md-6" style="float: left;">
+				<?=
+				TabsX::widget([
+					'items'=>$item,
+					'position'=>TabsX::POS_ABOVE,
+					'encodeLabels'=>false
+				])
+				?>
 			</div>
 		</div>
 	</div>
-
 <div class="col-xs-12">
 	<?php $this->registerJsFile(Yii::$app->request->baseUrl.'/js/profile-index.js'); ?>
 </div>
