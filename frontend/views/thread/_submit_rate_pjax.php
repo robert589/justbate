0c68376d957f7890d73394e89bbca8af7fb2b2a0
MaX121 <admin@max-metal.us>
2016-02-25 00:37:59 +0700

    [UPDATE] updating the star layout

--- frontend/views/thread/_submit_rate_pjax.php
+++ frontend/views/thread/_submit_rate_pjax.php
@@ -16,32 +16,32 @@ use kartik\widgets\StarRating;
 
 <!--Rating Part-->
 <div class="row">
-    <div class="col-xs-6" style="text-align: right;">
-        <!--Rating form part-->
-        <?= Html::beginForm('submit-rate', 'post', ['id'=>"ratingForm" ,'data-pjax' => '#submitRating', 'class' => 'form-inline']); ?>
-            <?= Html::hiddenInput('thread_id', $thread_id) ?>
-            <!-- Current Rating -->
-            <?= StarRating::widget([
-                'name' => 'userThreadRate',
-                'id' => 'thread_rating',
-                'value' => $avg_rating,
-                'readonly' => false,
-                'pluginOptions' => [
-                    'showCaption' => true,
-                    'min' => 0,
-                    'max' => 5,
-                    'step' => 1,
-                    'size' => 'xs',
-                ],
-                'pluginEvents' => [
-                ]
-                ])?>
-            </div>
-            <div class="col-xs-6" style="text-align: left; height: 100%;">
-                <span class="label label-primary"><?= $total_raters ?> Voter(s)</span>
-            </div>
-        <?= Html::endForm() ?>
-    </div>
-</div>
+    <div class="col-xs-12 col-md-6" style="float: right:">
+        <?= StarRating::widget([
+            'name' => 'userThreadRate',
+            'id' => 'thread_rating',
+            'value' => $avg_rating,
+            'readonly' => false,
+            'pluginOptions' => [
+                'showCaption' => true,
+                'min' => 0,
+                'max' => 5,
+                'step' => 1,
+                'size' => 'xs',
+            ],
+            'pluginEvents' => [
+            ]
+        ])
+        ?>
+        <?= Html::hiddenInput('thread_id', $thread_id) ?>
+    </div> <!-- div.col-xs-12 -->
+    <div class="col-xs-12 col-md-6" style="text-align: left;">
+        <span class="label label-primary ">
+            <?= $total_raters ?> Voter(s)
+        </span> <!-- span.label-primary -->
+    </div> <!-- div.col-xs-12.col-md-6 -->
+</div> <!-- div.row -->
+<?= Html::beginForm('submit-rate', 'post', ['id'=>"ratingForm" ,'data-pjax' => '#submitRating', 'class' => 'form-inline', 'style' => 'float: right;']); ?>
+<?= Html::endForm() ?>
 
 <?php Pjax::end();?>
