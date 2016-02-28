7ede5dfa5a4664c28da9e68ef79145356c167586
Peter <peterch1ang@yahoo.com>
2016-02-28 20:53:15 +0800

    delete comment form

---
+++ frontend/models/DeleteCommentForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace frontend\models;
+
+use yii\base\Model;
+
+use common\models\Comment;
+
+class DeleteCommentForm extends Model {
+	public $comment_id;
+
+	public function rules() {
+		return [['comment_id', 'integer'],
+			[['comment_id'], 'required']];
+	}
+
+	public function delete() {
+		if($this->validate()) {
+			$comment = Comment::findOne(['comment_id' => $this->comment_id]);
+			$comment->comment_status = 0;
+			$comment->update();
+		}
+	}
+
+	public function update() {
+		if($this->validate()) {
+			$comment = Comment::findOne(['comment_id' => $this->comment_id]);
+			$comment->comment_status = 0;
+			$comment->update();
+		}
+	}
+}
+
+?>
