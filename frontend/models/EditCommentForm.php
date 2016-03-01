07922e33b3353f5591ba78dc2482a873f7e1a9ea
Peter <peterch1ang@yahoo.com>
2016-03-01 19:51:55 +0800

    added parent_id must be integer

--- frontend/models/EditCommentForm.php
+++ frontend/models/EditCommentForm.php
@@ -12,38 +12,38 @@ use Yii;
  */
 class EditCommentForm extends Model
 {
-    public $comment;
-    public $parent_id;
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['comment', 'parent_id'] , 'required'],
-       ];
-    }
+	public $comment;
+	public $parent_id;
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['parent_id'], 'integer'],
+			[['comment', 'parent_id'] , 'required'],
+	   ];
+	}
 
-    /**
-     * Signs user up.
-     *
-     * @return User|null the saved model or null if saving fails
-     */
-    public function update()
-    {
-        if ($this->validate()) {
+	/**
+	 * Signs user up.
+	 *
+	 * @return User|null the saved model or null if saving fails
+	 */
+	public function update()
+	{
+		if ($this->validate()) {
 
-            $comment = Comment::findOne($this->parent_id);
-        
-            $comment->update_at = new Expression('NOW()');
-            $comment->comment = $this->comment;
-            if($comment->save()){
-                return true;
-            }
+			$comment = Comment::findOne($this->parent_id);
+			$comment->update_at = new Expression('NOW()');
+			$comment->comment = $this->comment;
+			if($comment->save()){
+				return true;
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        return null;
-    }
+		return null;
+	}
 }
