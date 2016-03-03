90943811381ae121ea50c48dab4362ab5fef1b6c
MaX121 <admin@max-metal.us>
2016-03-03 14:23:09 +0700

    [REPAIR] javascript broken script

--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -68,8 +68,7 @@ $(document).ready(function() {
         .find('#uploadProfilePicModal')
         .load($(this).attr("value"));
     }
-
-    $("#li#notif-expansion").click(function() {
+    $("li#notif-expansion").click(function() {
         //the reason of length 0 instead of 1 because this code is executed first then the notifcation is opened
         if($('#notif-expansion').parent().find('.open').length == 0){
             $("#notification-form").submit();
