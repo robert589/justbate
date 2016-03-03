7fe76a2a3ae0786a3631fceb0f1930531f8bbbe0
MaX121 <admin@max-metal.us>
2016-03-03 14:10:06 +0700

    [REPAIR] javascript broken script

--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     //on profile/index
     $("img#avatar").mouseenter(function() {
         $("img#avatar").css("opacity",".5");
@@ -23,83 +22,70 @@ $(document).ready(function() {
 
     $("#loginMenu").click(function(){
         $("#loginModal").modal("show")
-            .find('#loginModal')
-            .load($(this).attr("value"));
+        .find('#loginModal')
+        .load($(this).attr("value"));
     });
 
-    // $("button#create-button").mouseenter(function(){
-    //     $('span#create-button-label').css("text-decoration","underline");
-    // });
-    //
-    // $("button#create-button").mouseleave(function(){
-    //     $('span#create-button-label').css("text-decoration","none");
-    // });
     $("div#create-thread-dropdown").click(function() {
         $("div#create-thread-form").slideToggle("fast");
         $("span#icon-dropdown").toggleClass("glyphicon-chevron-down");
         $("span#icon-dropdown").toggleClass("glyphicon-chevron-up");
     });
 
-})
-
-//site_home
-$(document).on('click', '#comment_hide_list_thread_btn', function(){
-    if($(this).text() == 'Comment'){
-        $("#list_thread_comment_part").show();
-        $('#comment_hide_list_thread_btn').text('Hide');
-
-    }
-    else{
-        $("#list_thread_comment_part").hide();
-        $('#comment_hide_list_thread_btn').text('Comment');
-    }
-});
-$(document).on('click', '#edit_thread', function(){
-    $("#shown_part").hide();
-    $("#edit_part").show();
-});
-
-$("#delete_thread").on("click", function() {
-    krajeeDialog.confirm("Are you sure you want to proceed?", function (result) {
-        if (result) {
-            $("#delete_thread_form").submit();
-            return false;
+    // site_home
+    $("#comment_hide_list_thread_btn").click(function(){
+        if($(this).text() == 'Comment'){
+            $("#list_thread_comment_part").show();
+            $('#comment_hide_list_thread_btn').text('Hide');
+        } else {
+            $("#list_thread_comment_part").hide();
+            $('#comment_hide_list_thread_btn').text('Comment');
         }
     });
-});
+
+    $("#edit_thread").click(function(){
+        $("#shown_part").hide();
+        $("#edit_part").show();
+    });
+
+    $("#delete_thread").on("click", function() {
+        krajeeDialog.confirm("Are you sure you want to proceed?", function (result) {
+            if (result) {
+                $("#delete_thread_form").submit();
+                return false;
+            }
+        });
+    });
 
 
-$(document).on('click', '#cancel_edit_thread_button', function(){
-    $("#shown_part").show();
-    $("#edit_part").hide();
-});
+    $("#cancel_edit_thread_button").click(function(){
+        $("#shown_part").show();
+        $("#edit_part").hide();
+    });
 
-
-
-function beginProfilePicModal(){
-    $("#uploadProfilePicModal").modal("show")
+    function beginProfilePicModal(){
+        $("#uploadProfilePicModal").modal("show")
         .find('#uploadProfilePicModal')
         .load($(this).attr("value"));
-}
-
-$(document).click(function() {
-    //the resason of length 0 instead of 1 because this code is executed first then the notifcation is opened
-    if($('#notif-expansion').parent().find('.open').length == 0){
-        $("#notification-form").submit();
     }
-});
 
+    $("#li#notif-expansion").click(function() {
+        //the reason of length 0 instead of 1 because this code is executed first then the notifcation is opened
+        if($('#notif-expansion').parent().find('.open').length == 0){
+            $("#notification-form").submit();
+        }
+    });
 
-
-
-function beginLoginModal(){
-    $("#loginModal").modal("show")
+    function beginLoginModal(){
+        $("#loginModal").modal("show")
         .find('#loginModal')
         .load($(this).attr("value"));
-}
+    }
 
-$(document).on('click', '#login_link', function(){
-    beginLoginModal();
-    return false;
-});
+    $("#login_link").click(function(){
+        beginLoginModal();
+        return false;
+    });
 
+
+})
