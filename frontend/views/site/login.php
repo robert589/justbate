612bbaaf6e2e7fe69c6e3e7fd57eee529c1697fb
VMuliadi <admin@max-metal.us>
2016-04-17 13:04:05 +0700

    [UPDATE] Changing facebook login view

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -16,29 +16,30 @@ if(empty($redirect_from)){
     <h1><?= Html::encode('Login') ?></h1>
 
     <?php $form = ActiveForm::begin(['action' => ['site/login'], 'method' => 'post']) ?>
-        <div class="row" id="login-form">
-            <div class="col-xs-12"><?= $form->field($login_form, 'email')->textInput(['placeholder' => 'Your Username']) ?></div>
-            <div class="col-xs-12"><?= $form->field($login_form, 'password')->passwordInput(['placeholder' => 'Your Password']) ?></div>
-        </div>
+    <div class="row" id="login-form">
+        <div class="col-xs-12"><?= $form->field($login_form, 'email')->textInput(['placeholder' => 'Your Username']) ?></div>
+        <div class="col-xs-12"><?= $form->field($login_form, 'password')->passwordInput(['placeholder' => 'Your Password']) ?></div>
+    </div>
 
-        <div class="row">
-            <div class="col-xs-12">
-                <?= Html::submitButton('Sign in', ['class' => 'btn btn-primary', 'id' => 'sign-up']) ?>
-            </div>
-            <div class="col-xs-12">
-                <div class="input-group" style="width: 100%; border: 0;">
-                    <span class="input-group-addon"><?= $form->field($login_form,'rememberMe')->checkbox() ?></span>
-                </div>
-            </div>
-        </div><hr />
-        <div class="row" align="center">
-            <div id="social-icon">
-                <?= yii\authclient\widgets\AuthChoice::widget([
-                    'baseAuthUrl' => ['site/auth']
-                ]) ?>
+    <div class="row">
+        <div class="col-xs-12">
+            <?= Html::submitButton('Sign in', ['class' => 'btn btn-primary', 'id' => 'sign-up']) ?>
+        </div>
+        <div class="col-xs-12">
+            <div class="input-group" style="width: 100%; border: 0;">
+                <span class="input-group-addon"><?= $form->field($login_form,'rememberMe')->checkbox() ?></span>
             </div>
         </div>
+    </div><hr />
+    <div class="row" align="center">
+        <a href="/startUp/site/auth?authclient=facebook" data-popup-width="860" data-popup-height="480">
+            <div class="input-group" id="social-icon">
+                <span class="input-group-addon"><i class="fa fa-facebook" aria-hidden="true"></i></span>
+                <input class="form-control" id='social-icon-text' type='text' readonly='true' value='Login with Facebook' />
+            </div>
+        </a>
+    </div>
 
-        <?= Html::hiddenInput('redirect_from', $redirect_from) ?>
+    <?= Html::hiddenInput('redirect_from', $redirect_from) ?>
     <?php ActiveForm::end() ?>
 </div>
