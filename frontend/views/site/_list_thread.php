b8a2424a8a9ba59499ae823f64f6cf874e5a9e3a
VMuliadi <admin@max-metal.us>
2016-05-15 12:22:37 +0700

    Fixing layout for comment

--- frontend/views/site/_list_thread.php
+++ frontend/views/site/_list_thread.php
@@ -18,18 +18,17 @@ use common\models\User;
 		<div class="col-xs-12 thread-link">
 			<div class="col-xs-10 thread-title-list"><?= Html::a(Html::encode($model['title']), Yii::$app->request->baseUrl . '/thread/' . $model['thread_id'] . '/' . str_replace(' ' , '-', strtolower($model['title'])))?></div>
 			<div class="col-xs-2" style="font-size: 12pt; text-align: right;">
-				<!-- Your share button code -->
 				<div class="fb-share-button" data-href="http://justbate.com/thread/<?= $model['thread_id'] ?>/<?= str_replace(' ' , '-', strtolower($model['title'])) ?>" data-layout="button_count"></div>
 			</div>
 		</div>
 		<div class="user-comment-reaction col-xs-12">
 			<?php if($comment != null || $comment != false){ ?>
-						<div class="col-xs-1" style="width: 50px; margin-left: -15px; margin-bottom: 10px;" style="padding-left: 0; padding-right: 0;"><img src="<?= Yii::getAlias('@image_dir') . '/'. User::findOne(Yii::$app->getUser()->id)->photo_path ?>" height="20px;" /></div>
-						<div class="col-xs-11" style="margin-bottom: 10px;"><div class="name-link inline"><?= $comment['first_name'] . ' '. $comment['last_name']  ?> &nbsp; chooses <b><?= $comment['choice_text'] ?></b></div></div>
-				<?= HtmlPurifier::process($comment['comment']) ?>
+						<div class="col-xs-1" style="margin-left: -15px; margin-bottom: 10px; margin-right: -15px;" style="padding-left: 0; padding-right: 0;"><img src="<?= Yii::getAlias('@image_dir') . '/'. User::findOne(Yii::$app->getUser()->id)->photo_path ?>" class="img img-circle" height="50px" width="50px" /></div>
+						<div class="col-xs-offset-1 col-xs-11" style="margin-top: -68px; margin-left: 55px;"><div class="name-link inline"><?= $comment['first_name'] . ' '. $comment['last_name'] ?> &nbsp; chooses <?= $comment['choice_text'] ?></div></div>
+						<div class="col-xs-offset-1 col-xs-11" style="padding-left: 0; margin-left: 70px; margin-top: -40px;" ><?= HtmlPurifier::process($comment['comment']) ?></div>
 				<?php }else{ ?>
-					<?= HtmlPurifier::process($model['description']) ?>
-					<?php } ?><hr style="margin-top: 5px; margin-bottom: 5px;" />
+					<div class="col-xs-11" style="margin-bottm: 10px;"><?= HtmlPurifier::process($model['description']) ?></div>
+					<?php } ?>
 				</div>
 				<div class="col-xs-12 home-comment-tab">
 					<?= $this->render('_list_thread_bottom', [
