28789ec4b6dbe9a3df4e37ede94b0a8e290fa8ff
VMuliadi <admin@max-metal.us>
2016-05-15 15:52:48 +0700

    Replacing default.png

--- frontend/views/thread/_child_comment.php
+++ frontend/views/thread/_child_comment.php
@@ -26,40 +26,41 @@ use kartik\dialog\Dialog;
     ],
 
 
-]) ?>
+    ]) ?>
 
-<?= Dialog::widget(); ?>
+    <?= Dialog::widget(); ?>
 
-<div class="col-md-8">
-    <?php if($belongs){ ?>
+    <div class="col-md-8">
+        <?php if($belongs){ ?>
 
-        <?= Html::button('Edit', ['class' => 'btn btn-primary inline edit_comment', 'data-service' => $comment_id]) ?>
-        <?= Html::button('Delete', ['class' => 'btn btn-danger inline delete_comment', 'data-service' => $comment_id]) ?>
-    <?php } ?>
+            <?= Html::button('Edit', ['class' => 'btn btn-primary inline edit_comment', 'data-service' => $comment_id]) ?>
+            <?= Html::button('Delete', ['class' => 'btn btn-danger inline delete_comment', 'data-service' => $comment_id]) ?>
+            <?php } ?>
 
-    <?= Html::a("Comment",
-        Yii::$app->request->baseUrl . '/thread/get-child-comment?thread_id=' . $thread_id . '&comment_id=' . $comment_id ,
-        ['class' => 'button-like-link inline retrieve-child-comment-link',
+            <?= Html::a("Comment",
+            Yii::$app->request->baseUrl . '/thread/get-child-comment?thread_id=' . $thread_id . '&comment_id=' . $comment_id ,
+            ['class' => 'button-like-link inline retrieve-child-comment-link',
             'data-pjax' => "#child_comment_$thread_id",
             'data-service' => $comment_id, 'style' => 'margin-left:15px']) ?>
 
-    <?= Html::hiddenInput('child_comment_url', \yii\helpers\Url::to(['thread/get-sse-child-comment', 'comment_id' => $comment_id,
-        'last_time' =>  time() , ['id' => 'child_comment_url_' . $comment_id]   ])) ?>
-</div>
+            <?= Html::hiddenInput('child_comment_url', \yii\helpers\Url::to(['thread/get-sse-child-comment', 'comment_id' => $comment_id,
+            'last_time' =>  time() , ['id' => 'child_comment_url_' . $comment_id]   ])) ?>
+        </div>
 
 
-<div  align="center" class="col-xs-12" >
-    <?= Html::img(Yii::$app->request->baseUrl . '/frontend/web/img/loading.gif', ['style' => 'display:none;max-height:50px' , 'id' => 'child_comment_loading_gif_' . $comment_id ]) ?>
-</div>
+        <div  align="center" class="col-xs-12" >
+            <?= Html::img(Yii::$app->request->baseUrl . '/frontend/web/img/loading.gif', ['style' => 'display:none;max-height:50px' , 'id' => 'child_comment_loading_gif_' . $comment_id ]) ?>
+        </div>
 
 
 
 
-<?php if($retrieved){ ?>
+        <?php if($retrieved){ ?>
 
-<div class="col-xs-12" style="background-color: #dff0d8; " id="<?= 'comment_part_' . $comment_id ?>">
-    <div class="col-xs-12" style="margin-top: 15px;" >
-        <?= $this->render('_child_comment_input_box', ['comment_id' => $comment_id, 'child_comment_form' => $child_comment_form]) ?>
+            <div class="col-xs-12" style="background-color: #dff0d8; " id="<?= 'comment_part_' . $comment_id ?>">
+                <div class="col-xs-12" style="margin-top: 15px;" ><?= $this->render('_child_comment_input_box', ['comment_id' => $comment_id, 'child_comment_form' => $child_comment_form]) ?></div>
+            </div>
+
             <div class="col-xs-12 text-center">
                 <div id="child_comment_sse_<?= $comment_id ?>" ></div>
                 <?= ListView::widget([
@@ -71,21 +72,19 @@ use kartik\dialog\Dialog;
                     'itemView' => function ($model, $key, $index, $widget) {
                         $comment_vote_form = CommentVote::getCommentVotesOfComment($model['comment_id'], Yii::$app->getUser()->getId());
                         return $this->render('_listview_child_comment',['model' => $model, 'total_like' => $comment_vote_form['total_like'],
-                                        'total_dislike' => $comment_vote_form['total_dislike'], 'vote' => $comment_vote_form['vote']]);
+                        'total_dislike' => $comment_vote_form['total_dislike'], 'vote' => $comment_vote_form['vote']]);
                     }
-                ]) ?>
-            </div>
-        </div>
-</div>
+                    ]) ?>
+                </div>
 
 
-<?php } ?>
+                <?php } ?>
 
 
-<?php $form = ActiveForm::begin(['action' => ['thread/delete-comment'], 'method' => 'post', 'id' => 'delete_comment_form_' . $comment_id]) ?>
-    <?= Html::hiddenInput('comment_id', $comment_id) ?>
-    <?= Html::hiddenInput('thread_id', $thread_id) ?>
+                <?php $form = ActiveForm::begin(['action' => ['thread/delete-comment'], 'method' => 'post', 'id' => 'delete_comment_form_' . $comment_id]) ?>
+                <?= Html::hiddenInput('comment_id', $comment_id) ?>
+                <?= Html::hiddenInput('thread_id', $thread_id) ?>
 
-<?php ActiveForm::end() ?>
+                <?php ActiveForm::end() ?>
 
-<?php Pjax::end() ?>
+                <?php Pjax::end() ?>
