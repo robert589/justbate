69145bbe6a35612ee579be8cd085c79844b9d3d8
Vinsen Muliadi <admin@max-metal.us>
2016-07-08 20:41:06 +0700

    Resizing profile picture on comment and remove background on notification

--- frontend/views/comment/child-comment.php
+++ frontend/views/comment/child-comment.php
@@ -20,12 +20,12 @@ $comment = Html::encode($child_comment->getComment());
 <article>
     <div class="col-xs-12" style="margin-bottom:10px">
         <div class="inline">
-            <img class="img img-rounded profile-picture-comment" style="height:35px;width:35px" src=<?= $commentator_user_photo_path_link ?>>
+            <img class="img img-rounded profile-picture-comment" style="height:60px; height: 60px;" src=<?= $commentator_user_photo_path_link ?>>
         </div>
         <div class="inline commentator-name" style="margin-left: 15px" >
             <?= Html::a("<b>" .Html::encode($comment_creator_full_name) .  "</b>", $commentator_user_profile_link )?>
         </div>
-        <div  style="margin-left: 10px" class="inline" class="commentator-moderate">
+        <div class="inline" style="margin-left: 10px" class="commentator-moderate">
             <?= $this->render('child-comment-votes', ['comment' => $child_comment, 'is_thread_comment' => false]) ?>
         </div>
     </div>
