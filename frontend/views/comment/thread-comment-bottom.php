f83f61bc0aa361e3be667bedbf428ebc3e5cbd77
Vinsen Muliadi <admin@max-metal.us>
2016-07-09 20:09:04 +0700

    Closing menu when menu item clicked

--- frontend/views/comment/thread-comment-bottom.php
+++ frontend/views/comment/thread-comment-bottom.php
@@ -13,40 +13,40 @@ $belongs_to_current_user = $thread_comment->isBelongToCurrentUser();
 
 <div class="col-xs-12 commentator-comment">
     <?= $this->render('comment-section', ['thread_comment' => $thread_comment,
-    'edit_comment_form' => new \frontend\models\EditCommentForm(),
-    'is_thread_comment' => true  ]) ?>
-</div>
+        'edit_comment_form' => new \frontend\models\EditCommentForm(),
+        'is_thread_comment' => true  ]) ?>
+    </div>
 
-<div class="inline" class="comment-votes">
-    <?= $this->render('comment-votes', [ 'comment' => $thread_comment ,
-    'is_thread_comment' => $is_thread_comment])?>
-</div>
+    <div class="inline" class="comment-votes">
+        <?= $this->render('comment-votes', [ 'comment' => $thread_comment ,
+        'is_thread_comment' => $is_thread_comment])?>
+    </div>
 
-<div class="inline">
-    <?= Html::a("Comment",    $child_comment_request_url,
-    ['class' => 'btn btn-sm btn-primary inline retrieve-child-comment-link',
-    'data-pjax' => "#child_comment_$thread_id",
-    'data-service' => $comment_id,
-    'style' => 'margin-left:15px; float:left'])?>
-    <?php if($belongs_to_current_user) { ?>
-        <div class="inline" style="margin-left: 15px;">
-            <input type="checkbox" id="dropdown-comment-input-<?= $comment_id ?>" />
-            <label for="dropdown-comment-input-<?= $comment_id ?>"><span class="glyphicon glyphicon-chevron-down"></span></label>
-            <table id="user-table-comment-<?= $comment_id ?>">
-                <tbody>
-                    <tr><td><button data-service="<?= $comment_id ?>" class="edit_comment inner btn btn-block btn-default">Edit</button></td></tr>
-                    <tr><td><button data-service="<?= $comment_id ?>" class="delete-comment inner btn btn-block btn-danger">Delete</button></td></tr>
-                </tbody>
-            </table>
+    <div class="inline">
+        <?= Html::a("Comment",    $child_comment_request_url,
+            ['class' => 'btn btn-sm btn-primary inline retrieve-child-comment-link',
+            'data-pjax' => "#child_comment_$thread_id",
+            'data-service' => $comment_id,
+            'style' => 'margin-left:15px; float:left'])?>
+        <?php if($belongs_to_current_user) { ?>
+            <div class="inline" style="margin-left: 15px;">
+                <input type="checkbox" id="dropdown-comment-input-<?= $comment_id ?>" />
+                <label for="dropdown-comment-input-<?= $comment_id ?>"><span class="glyphicon glyphicon-chevron-down"></span></label>
+                <table id="user-table-comment-<?= $comment_id ?>" data-service="<?= $comment_id ?>">
+                    <tbody>
+                        <tr><td><button data-service="<?= $comment_id ?>" class="edit_comment inner btn btn-block btn-default">Edit</button></td></tr>
+                        <tr><td><button data-service="<?= $comment_id ?>" class="delete-comment inner btn btn-block btn-danger">Delete</button></td></tr>
+                    </tbody>
+                </table>
+            </div>
+            <?php } ?>
         </div>
-    <?php } ?>
-    </div>
-    <div  align="center" class="col-xs-12" >
-        <?= Html::img(Yii::$app->request->baseUrl . '/frontend/web/img/loading.gif',
-        ['style' => 'display:none;max-height:50px' ,
-        'id' => 'child_comment_loading_gif_' . $comment_id])?>
-    </div>
+        <div  align="center" class="col-xs-12" >
+            <?= Html::img(Yii::$app->request->baseUrl . '/frontend/web/img/loading.gif',
+                ['style' => 'display:none;max-height:50px' ,
+                'id' => 'child_comment_loading_gif_' . $comment_id])?>
+            </div>
 
-<?= $this->render('child-comment-list', ['thread_comment' => $thread_comment,
-                                        'retrieved' => false,
-                                        'child_comment_form' => new \frontend\models\ChildCommentForm() ]) ?>
+            <?= $this->render('child-comment-list', ['thread_comment' => $thread_comment,
+                'retrieved' => false,
+                'child_comment_form' => new \frontend\models\ChildCommentForm() ]) ?>
