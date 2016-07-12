78b71a48b807f65123b7d7e382e7bb4ebcef4a49
Vinsen Muliadi <admin@max-metal.us>
2016-07-12 13:32:23 +0700

    Remake login form

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -1,16 +1,16 @@
  <?php
 
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \frontend\models\SignupForm */
+ /* @var $this yii\web\View */
+ /* @var $form yii\bootstrap\ActiveForm */
+ /* @var $model \frontend\models\SignupForm */
 
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-use dektrium\user\widgets\Connect;
+ use yii\helpers\Html;
+ use yii\bootstrap\ActiveForm;
+ use dektrium\user\widgets\Connect;
 
-$this->title = 'Login';
-?>
-<div class="site-login" style="background-color: white">
+ $this->title = 'Login';
+ ?>
+<!-- <div class="site-login" style="background-color: white">
     <div class="row" id="signup-wrapper">
         <div align="center">
             <img src="<?= Yii::$app->request->baseUrl . '/frontend/web/img/logo.png' ?>"
@@ -54,6 +54,50 @@ $this->title = 'Login';
                         <?= Html::submitButton('Sign in', ['class' => 'btn btn-primary', 'id' => 'sign-up']) ?>
                     </div>
                 </div>
+            <?php ActiveForm::end() ?>
+        </div>
+    </div>
+</div> -->
+<div class="container-fluid">
+    <div class="col-md-6 col-xs-9" id="justbate-logo-wrapper">
+        <img src="<?= Yii::$app->request->baseUrl . '/frontend/web/img/logo.png' ?>" style="height:130px"/>
+    </div>
+    <div class="col-md-6 col-xs-9" id="login-register-form">
+        <div class="col-md-6 col-xs-12" id="register-form">
+            <a href="https://www.justbate.com/site/auth?authclient=facebook">
+                <span class="input-group register-data">
+                    <span class="input-group-addon"><span class="fa fa-facebook"></span></span>
+                    <input type="text" class="form-control" value="Sign Up With Facebook" readonly="true" />
+                </span>
+            </a>
+
+            <a href="<?= Yii::$app->request->baseUrl . '/signup' ?>"">
+                <span class="input-group register-data">
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+                    <input type="text" class="form-control" value="Sign Up With Email" readonly="true" />
+                </span>
+            </a>
+            <hr class="hide-sm" />
+            <button id="lupa-password" class="hide-sm btn btn-block btn-danger">Forgot Password</button>
+            <hr class="hide-md" />
+        </div>
+
+        <div class="col-md-6 col-xs-12" id="login-form">
+            <?php $form = ActiveForm::begin(['action' => ['site/login'], 'method' => 'post']) ?>
+            <div class="col-xs-12">
+                <span class="input-group login-data">
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                    <?= $form->field($login_form, 'email')->textInput(['placeholder' => 'Your Email'], ['class' => 'form-control']) ?>
+                </span>
+
+                <span class="input-group login-data">
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                    <?= $form->field($login_form, 'password')->passwordInput(['placeholder' => 'Your Password'], ['class' => 'form-control']) ?>
+                </span>
+                <hr />
+                <button id="button-login" class="btn btn-block btn-primary">Login</button>                      
+                <button id="lupa-password" class="btn btn-block btn-danger hide-md">Forgot Password</button>
+            </div>
             <?php ActiveForm::end() ?>
         </div>
     </div>
