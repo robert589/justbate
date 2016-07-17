af5f3908af7075a7df9e97afdab2090c16aca5d9
Robert Limanto <r_limanto@hotmail.com>
2016-07-17 17:16:09 +0800

    super fast child comment 2

--- frontend/views/comment/child-comment-input-box.php
+++ frontend/views/comment/child-comment-input-box.php
@@ -14,14 +14,18 @@ $last_comment_id_current_user = isset($last_comment_id_current_user) ? $last_com
 
 <div class="col-xs-12">
     <div class="col-xs-12">
+        <?= Html::textarea('child-comment-input-box-text-area-'.$comment_id, null,[
             'id' => 'child-comment-input-box-text-area-' . $comment_id,
             'rows' => 3,
             'cols' => 3,
             'style' => 'width:100%',
             'placeholder' => 'add comment here..' ])
+         ?>
     </div>
     <div class="col-xs-offset-6 col-xs-6">
         <?= Html::button('Submit',
+            ['class' => 'btn btn-sm btn-primary child-comment-input-box-submit-button',
+                'style' => 'float:right',
                 'data-service' => $comment_id,
                 'id' => 'submit-child-comment-submit-button-'  . $comment_id
             ]) ?>
